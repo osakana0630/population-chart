7d5c2050d80c11dc15b850ab46eceaf21c877ed8
imai0630 <naoya.imai@aitravel.jp>
2022-09-25 01:56:02 +0900

    chart描画 wip

--- src/components/core/Charts/ChartsHome/ChartsHome.tsx
+++ src/components/core/Charts/ChartsHome/ChartsHome.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useState, ChangeEvent } from 'react'
+import { FC, useEffect, useState, ChangeEvent, useMemo } from 'react'
+import { Chart } from 'react-google-charts'
 import {
   Grid,
   Checkbox,
@@ -18,9 +19,16 @@ type Prefecture = {
 
 const baseUrl = 'https://opendata.resas-portal.go.jp/api/v1/'
 
+type PopulationData = {
+  [prefCode: number]: { year: number; value: number }[]
+}
+
 const ChartsHome: FC<Props> = ({}) => {
   const [prefectures, setPrefectures] = useState<Prefecture[]>([])
   const [selectedPrefCode, setSelectedPrefCode] = useState<number[]>([])
+  const [populationData, setPopulationData] = useState<PopulationData | null>(
+    null,
+  )
 
   // 都道府県取得
   useEffect(() => {
@@ -33,6 +41,32 @@ const ChartsHome: FC<Props> = ({}) => {
       })
   }, [])
 
+  // 人口数取得
+  useEffect(() => {
+    if (!selectedPrefCode.length) return
+    selectedPrefCode.forEach((prefCode) => {
+      if (
+        populationData &&
+        Object.keys(populationData).includes(String(prefCode))
+      ) {
+        return
+      }
+
+      axios
+        .get(baseUrl + `population/composition/perYear?&prefCode=${prefCode}`, {
+          headers: {
+            'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY as string,
+          },
+        })
+        .then((res) => {
+          setPopulationData((prev) => ({
+            ...prev,
+            [prefCode]: res.data.result.data[0].data,
+          }))
+        })
+    })
+  }, [populationData, selectedPrefCode])
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const targetCode = Number(event.target.value)
     if (event.target.checked) {
@@ -41,12 +75,57 @@ const ChartsHome: FC<Props> = ({}) => {
         ...Array.from(new Set([...prev, targetCode])),
       ])
     } else {
+      // チェック外した時
+      // 該当都道府県のチャートデータを削除する
       setSelectedPrefCode((prev) => prev.filter((code) => code !== targetCode))
+      const copyPopulationData = { ...populationData }
+      delete copyPopulationData[targetCode]
+      setPopulationData(copyPopulationData)
     }
   }
 
   const isSelected = (prefCode: number) => {
     return selectedPrefCode.includes(prefCode)
+  }
+
+  const createChartData = (): any => {
+    if (!selectedPrefCode.length) {
+      return [
+        ['年', 'dummy'],
+        ['2022', 0],
+      ]
+    }
+
+    const firstRow = ['年']
+    prefectures.forEach((pref) => {
+      if (selectedPrefCode.includes(pref.prefCode)) {
+        firstRow.push(pref.prefName)
+      }
+    })
+
+    const dataRow: any = []
+    Object.values(populationData || {}).forEach((population, index) => {
+      population.forEach((p, i) => {
+        if (index === 0) {
+          dataRow.push([String(p.year), p.value])
+        } else {
+          dataRow[i].push(p.value)
+        }
+      })
+    })
+
+    dataRow.unshift(firstRow)
+
+    return dataRow
+  }
+
+  const chartData = createChartData()
+  console.log(chartData)
+
+  const options = {
+    title: '年別人口数',
+    curveType: 'function',
+    legend: { position: 'bottom' },
   }
 
   return (
@@ -81,7 +160,15 @@ const ChartsHome: FC<Props> = ({}) => {
       </Grid>
 
       {/* chart 部分*/}
-      <Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="400px"
+          data={chartData}
+          options={options}
+        />
+      </Grid>
     </Grid>
   )
 }
